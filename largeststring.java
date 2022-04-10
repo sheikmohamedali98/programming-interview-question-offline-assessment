916edeae512def743dcdfa4c27a2336e4b3c0f43
sheikmohamedali98 <103345551+sheikmohamedali98@users.noreply.github.com>
2022-04-10 10:08:56 +0530

    third commit

---
+++ largeststring.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class LargestPossibleString
+{
+    public void largest_string(String string1,String string2)
+    {
+        char[] letter1=string1.toCharArray();
+        char[] letter2=string2.toCharArray();
+        for(int i=0;i<letter1.length;i++)
+        {
+            for(int j=i+1;j<letter1.length;j++)
+            {
+                if(letter1[i]<letter1[j])
+                {
+                    char temp=letter1[j];
+                    letter1[j]=letter1[i];
+                    letter1[i]=temp;
+                }
+            }
+        }
+        for(int i=0;i< letter2.length;i++)
+        {
+            for(int j=i+1;j< letter2.length;j++)
+            {
+                if(letter2[i]<letter2[j])
+                {
+                    char temp=letter2[j];
+                    letter2[j]=letter2[i];
+                    letter2[i]=temp;
+                }
+            }
+        }
+        System.out.println("String 1: "+String.valueOf(letter1));
+        System.out.println("String 2: "+String.valueOf(letter2));
+    }
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number of String to be Entered: ");
+        int number=scan.nextInt();
+        System.out.println("String 1: ");
+        String string1=scan.next();
+        System.out.println("String 2: ");
+        String string2=scan.next();
+        LargestPossibleString largest=new LargestPossibleString();
+        largest.largest_string(string1,string2);
+    }
+}
