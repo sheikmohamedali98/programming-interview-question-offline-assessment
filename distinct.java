a032ba9696a18aca00c7de27057608b6ea5ee60c
sheikmohamedali98 <103345551+sheikmohamedali98@users.noreply.github.com>
2022-04-10 10:27:26 +0530

    seventh commit

---
+++ distinct.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class dublicateremove {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter The String:");
+		String str=in.nextLine();
+		Set<String> temp= new LinkedHashSet<String>(); //this is not allow dublicate String
+		String[] arr=str.split(" ");
+		for(String store:arr){
+			temp.add(store);
+			
+		}
+		String result=temp.toString().replace("[", " ").replace("]", " ").replace(",", " ") ;//set we will print some character],[ like this
+		
+		
+System.out.println("The Distinct Word are: "+result);
+	}
+
+}
